88bf2b0cdca951690637d07c32a4d42d3a05fbd3
lofix <maciekk.milewski@gmail.com>
2019-07-03 21:55:52 +0200

    tidy up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,67 +1,19 @@
 import React, {Component} from 'react';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
 
-import WithClass from '../hoc/WithClass';
+import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
+
+import Layout from '../components/Layout/Layout';
 
 import './App.css';
 
 class App extends Component {
-  state = {
-    persons: [
-      {id:'fdsfsd', name:'Maciek', age:'28'},
-      {id:'fffgb', name:'Sebek', age:'28'},
-      {id:'r4r43', name:'Aza', age:'27'}
-    ],
-    showPersons: false
-  }
-
-  nameChangedHandler = ( event, id ) => {
-    const personIndex = this.state.persons.findIndex( p => {
-      return p.id === id;
-    });
-
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-    this.setState({persons: persons});
-  }
-
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex,1);
-    this.setState({persons: persons});
-  }
-
-  togglePersonHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
-  } 
-
   render () {
-    let persons = null;
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          <Persons
-            persons = {this.state.persons}
-            clicked = {this.deletePersonHandler}
-            changed = {this.nameChangedHandler}
-          />
-        </div>
-      );
-    }
     return (
-      <WithClass classes="App">
-        <Cockpit 
-          clicked={this.togglePersonHandler}
-          persons = {this.state.persons}/>
-        {persons}
-      </WithClass>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
     );
   }
   
