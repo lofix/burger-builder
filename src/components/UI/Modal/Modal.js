1c00f27c3532835a63769d459e08cff11f8d3c2c
lofix <maciekk.milewski@gmail.com>
2019-07-03 23:25:15 +0200

    implement Backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import Auxi from '../../../hoc/Auxi';
+import Backdrop from '../Backdrop/Backdrop';
+
 import modules from './Modal.module.css'
 
 const modal = (props) => {
-    return (  
-    <div 
-        className={modules.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-    </div>
+    return (
+    <Auxi>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div 
+            className={modules.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+            {props.children}
+        </div>
+    </Auxi>
     );
 }
 
